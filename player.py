a01d4e41b86ab9a60948484546b9374b44e0090f
Nughat <48729141+Nughat@users.noreply.github.com>
2021-04-11 17:51:17 -0400

    added comments

--- player.py
+++ player.py
@@ -100,35 +100,35 @@ class Ram2(Player):
     def __init__(self):
 
         self.player_name = "Ram2"
-
-    guessnum = 0
-    guessecondnum = 0
-    guesses = []
+    #globals
+    guessnum = 0 #to find 1st color
+    guessecondnum = 0 #to find 2nd color based on the 1st color
+    guesses = [] #to store all guesses
     
     def make_guess(self, board_length, colors, scsa, last_response):
         
-        if (last_response[2] == 0):
+        if (last_response[2] == 0): #at the beginning of a new round reset all globals
             self.guessnum = 0
             self.guessecondnum = 0
             self.guesses = []
-        if not self.guesses:
-            color = colors[self.guessnum]
-            guess = list_to_str(color*board_length)
-        if (self.guesses):
-            if (last_response[0] == 0 and last_response[1]== 0):
-                self.guessnum = self.guessnum+1
-                self.guessecondnum = self.guessnum #B = 1
-                color = colors[self.guessnum]
+        if not self.guesses: #try AAAA on the first guess
+            color = colors[self.guessnum] #guessnum has value 0
+            guess = list_to_str(color*board_length) #guess will be the 0th element of colors, which is A
+        if (self.guesses): #guesses that are not the first guess
+            if (last_response[0] == 0 and last_response[1]== 0): #no colors are correct
+                self.guessnum = self.guessnum+1 #try the next color in colors
+                self.guessecondnum = self.guessnum #update the var for the second color as well, since both colors will be found lexicographically (ex: BBBB has some correct colors, then the second color must be one that comes after B)
+                color = colors[self.guessnum] 
                 guess = list_to_str(color*board_length)
             elif (last_response[0] == 2): #2 colors in correct position
-                color = [colors[self.guessnum],colors[self.guessecondnum+1],colors[self.guessnum],colors[self.guessecondnum+1]]
-                guess = list_to_str(color)
-                self.guessecondnum = self.guessecondnum+1    
-            elif (last_response[0] == 0 and (last_response[1] == 4 or last_response[1] == 2)):
-                color = self.guesses[-1]
-                color.reverse()
+                color = [colors[self.guessnum],colors[self.guessecondnum+1],colors[self.guessnum],colors[self.guessecondnum+1]] #assume that the colors in the correct position are the 0th and 2nd colors, so change the 1st and 3rd elements 
+                guess = list_to_str(color) 
+                self.guessecondnum = self.guessecondnum+1 #change the second color alphabetically until it is found   
+            elif (last_response[0] == 0 and (last_response[1] == 4 or last_response[1] == 2)): #the assumption from the last condition causes all colors to be in wrong position, meaning that the 1st and 3rd elements were correct and the 0th and 2nd elements need to be changed
+                color = self.guesses[-1] #retrieve the previous guess
+                color.reverse() #reverse the previous guess
                 guess = list_to_str(color)
       
         #print(guess, " ", last_response)
-        self.guesses.append(color)
-        return guess
+        self.guesses.append(color) #store all guesses in a list in case they are needed later on
+        return guess #return the guess
